d4c649698df3cb6c183c09e6a1b41b251ca4277c
Teresa <tpan@unc.edu>
2022-08-31 20:59:54 -0400

    server

--- server.js
+++ server.js
@@ -16,7 +16,19 @@
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
+const http = require('http')
 
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('./public/index.html')
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
 
 
 
